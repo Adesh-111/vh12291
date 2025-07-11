421011d25f70e67f71d74a0955ff7b1e0ebcfbe9
Adesh-111 <adeshabinesh25@gmail.com>
2025-07-11 12:22:42 +0530

    update the shortcode redirect api

--- Backend Test Submission/index.js
+++ Backend Test Submission/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { forwardRef } from "react";
+
 
 const app = express();
 const port = 3000;
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 //logging middleware
 
-const BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhZGVzaGFiaW5lc2gyNUBnbWFpbC5jb20iLCJleHAiOjE3NTIyMTU3MDAsImlhdCI6MTc1MjIxNDgwMCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjM1OGMyYjYxLTRmNmEtNGM2Zi1hZTU0LWI5NDM4NGRhMmQ1YiIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImFkZXNoIGQiLCJzdWIiOiJkMmZhNzdhZS1iMzA1LTQzZmQtYTc4Ny1hOTMzOTNkNGQwNmMifSwiZW1haWwiOiJhZGVzaGFiaW5lc2gyNUBnbWFpbC5jb20iLCJuYW1lIjoiYWRlc2ggZCIsInJvbGxObyI6InZoMTIyOTEiLCJhY2Nlc3NDb2RlIjoiQ1dicWdLIiwiY2xpZW50SUQiOiJkMmZhNzdhZS1iMzA1LTQzZmQtYTc4Ny1hOTMzOTNkNGQwNmMiLCJjbGllbnRTZWNyZXQiOiJ5VGVuV3R0WFhTeWdZa3VLIn0.3h-CR2h7Uk_0mMqyqYTPGZsAEHeRd-I5ngZnuzIBTeg";
+const BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhZGVzaGFiaW5lc2gyNUBnbWFpbC5jb20iLCJleHAiOjE3NTIyMTY5MjgsImlhdCI6MTc1MjIxNjAyOCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6ImIxZmEzMDU5LTFjZTctNDcwZC1hYjBiLWM0MGEwZjhkMDI4ZCIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImFkZXNoIGQiLCJzdWIiOiJkMmZhNzdhZS1iMzA1LTQzZmQtYTc4Ny1hOTMzOTNkNGQwNmMifSwiZW1haWwiOiJhZGVzaGFiaW5lc2gyNUBnbWFpbC5jb20iLCJuYW1lIjoiYWRlc2ggZCIsInJvbGxObyI6InZoMTIyOTEiLCJhY2Nlc3NDb2RlIjoiQ1dicWdLIiwiY2xpZW50SUQiOiJkMmZhNzdhZS1iMzA1LTQzZmQtYTc4Ny1hOTMzOTNkNGQwNmMiLCJjbGllbnRTZWNyZXQiOiJ5VGVuV3R0WFhTeWdZa3VLIn0.rhdO9qj_Uum8y5X1HdTHYZkg0BGzDtKhx3ghcY94mGo";
 
 async function Log(stack, level, logPackage, message) {
   const logData = {
@@ -40,7 +40,7 @@ async function Log(stack, level, logPackage, message) {
   }
 }
 
-// Log("backend", "debug", "handler", "Founded the error");
+// Log("backend", "error", "handler", "Cannot convert object to primitive value");
 
 
 const urls = {};
@@ -88,10 +88,11 @@ app.post('/shorturls', (req, res) => {
 });
 
 app.get("/:shortcode", (req, res) =>{
-    const shortcode = req.params;
+    const {shortcode}= req.params;
     const data = urls[shortcode];
     const now  = new Date().toISOString()
-    if(now > data.expiry) res.json({
+    if(now > data.expiry) 
+        res.json({
         message : "The shorturl is expired"
     });
     status[shortcode].clicks += 1;
@@ -99,8 +100,10 @@ app.get("/:shortcode", (req, res) =>{
         time : now,
         referrer : req.headers.referer || null,
         location : req.headers['x-forwarded-for'] || null
-    })
-    res.json({ message: 'Redirecting to url', url: urlData.url });
+    });
+    console.log(status[shortcode]);
+    res.redirect(data.url);
+    res.json({ message: 'Redirecting to url', url: data.url });
 })
 
 app.get("/", (req, res) => {
